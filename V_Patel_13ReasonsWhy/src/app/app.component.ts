698f2e0ea967abb608aa1f811cfaa0601dfe5587
Vrund1133 <105453398+Vrund1133@users.noreply.github.com>
2023-04-22 16:37:09 -0400

    solved message error.

--- V_Patel_13ReasonsWhy/src/app/app.component.ts
+++ V_Patel_13ReasonsWhy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReasonswhyService } from './reasonswhy.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'V_Patel_13ReasonsWhy';
+  contentId: number = 4;
+  SelectedContent: any = {};
+
+  defaultimg: string = "https://resizing.flixster.com/qxCTpAWvvb6Y8cPJEFq_hafm5zc=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvUlRUVjI5NTc2Ni53ZWJw";
+
+  constructor(private ReasonswhyService: ReasonswhyService){}
+
+  logId(){
+    console.log(`ID: ${this.SelectedContent.id}`);
+    console.log(`Title: ${this.SelectedContent.title}`);
+
+
+  }
+
+  ngOnInit(){
+    this.ReasonswhyService.getContentById(this.contentId).subscribe(content => this.SelectedContent = content);
+  }
+
+  
+  showCharacterInfo(id: any, title: String) {
+    console.log({ id, title });
+  }
+
+  
 }
+
+
